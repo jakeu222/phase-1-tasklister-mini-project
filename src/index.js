0aea50e9c52be9a2e43643b268d621bafd40dbff
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-08-03 12:59:23 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const unordered = document.querySelector("#tasks")
+
+  const newTask = document.querySelector("#create-task-form")
+  newTask.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const newTask2 = event.target['new-task-description'].value
+
+    const newElement = document.createElement("li")
+    newElement.textContent = newTask2
+    unordered.append(newElement)
+
+
+  })
 });
